56dff558ed1d1d7660a45202fe051fe90e24d818
JulianaFigueira <juliana.f@samsung.com>
2016-11-25 09:36:27 -0400

    Initial Commit of Final Guardian

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1480080778,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/spritebackground-sheet0.png",
+		"images/blocos-sheet0.png",
+		"images/nave-sheet0.png",
+		"images/spriteball-sheet0.png",
+		"images/planet-sheet0.png",
+		"images/backgroundpageinitial-sheet0.png",
+		"images/jogar.png",
+		"images/highscore.png",
+		"media/142608__autistic-lucario__error.m4a",
+		"media/142608__autistic-lucario__error.ogg",
+		"media/49695__ejfortin__energy-whip-2.m4a",
+		"media/49695__ejfortin__energy-whip-2.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
